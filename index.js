6b46e0de492b84a68d4b146744397b18eb9025a4
Kelly Bazella <kelly.bazella@gmail.com>
2020-02-22 18:55:50 -0600

    finished product

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var inquirer = require("inquirer");
 var fs = require("fs");
 var axios = require("axios");
 var generateHTML = require("./generateHTML");
-
+const htmlPdf = require('html-pdf-chrome');
+const options = { format: "Letter", charset: "utf-8" }; 
 // function to ask questions
 // favorite color
 // github username
@@ -15,8 +16,10 @@ var generateHTML = require("./generateHTML");
 //github api call
 //.then call generateHTML and store in variable then pass var to pdf
 // function to convert HTML to PDF
+
 let userColor;
 let userName;
+
 inquirer
   .prompt([
     {
@@ -24,13 +27,13 @@ inquirer
       message: "What is your favorite color?",
       name: "color",
       choices: [
-        "Green",
+        "green",
         new inquirer.Separator(),
-        "Blue",
+        "blue",
         new inquirer.Separator(),
-        "Pink",
+        "pink",
         new inquirer.Separator(),
-        "Red"
+        "red"
       ]
     },
     {
@@ -46,18 +49,28 @@ inquirer
     axios.get(queryUrl).then(function(response) {
       var info = {
         color: userColor,
-        user: userName,
+        avi: response.data.avatar_url,
         followers: response.data.followers,
         repos:response.data.public_repos,
         following:response.data.following,
         bio:response.data.bio,
+        star: response.data.public_gists,
         fullname: response.data.name,
         company:response.data.company,
         location:response.data.location,
         linkedin:response.data.blog,
         github:response.data.url
       };
-      console.log(info)
-      generateHTML(info);
+     const htmlPage =  generateHTML(info);
+     fs.writeFile("profile.html", htmlPage, function(err){
+       if(err){
+         return console.log("Error!")
+       }else{
+         console.log("Success!")
+        htmlPdf.create(htmlPage, options).then((pdf) => pdf.toFile('profile.pdf'));
+        htmlPdf.create(htmlPage, options).then((pdf) => pdf.toBase64());
+        htmlPdf.create(htmlPage, options).then((pdf) => pdf.toBuffer());
+       }
+     })
     });
   });
